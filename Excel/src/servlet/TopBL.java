9b25778d8d66d694e6d66f5694a8fb89024035cd
hanainanami1198 <hanainanami1198@DESKTOP-HGUVAS4.ad.se-project.co.jp>
2020-10-05 15:27:13 +0900

    10/5、本社、Excel取得作成中

--- Excel/src/servlet/TopBL.java
+++ Excel/src/servlet/TopBL.java
@@ -1,5 +1,6 @@
 package servlet;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -7,6 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 /**
  * Servlet implementation class TopBL
@@ -26,16 +33,41 @@ public class TopBL extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    //エクセルをダウンロードするサーブレット
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		getServletContext().getRequestDispatcher("/NewFile.jsp").forward(request, response);
+		 //エクセルファイルへアクセスするためのオブジェクト
+	    Workbook excel = WorkbookFactory.create(new File("Sample.xlsx"));
+
+	    // シート名がわかっている場合
+	    Sheet sheet = excel.getSheet("Sheet1");
+
+	    //0行目
+	    Row row = sheet.getRow(0);
+
+	    //0番目のセル
+	    Cell cell = row.getCell(0);
+
+	    //文字列の取得
+	    String value = cell.getStringCellValue();
+
+	    //取得した文字列の表示
+	    System.out.println(value);
+		//ダウンロードページTop.jspを表示する
+		getServletContext().getRequestDispatcher("/Top.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		String filename = "Sample.xlsx";//Excelファイル名
+//		//レスポンスヘッダを設定
+//		response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");//ファイルのタイプを.xlsxに指定
+//        response.setHeader("content-disposition", String.format("attachment; filename=\"%s\"", filename));//ファイル名指定
+//        response.setCharacterEncoding("UTF-8");//ファイルの文字コード変換
+
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
